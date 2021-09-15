3dcc498f6735f7e0cb08b3b2cc77a3c3d8afe6ba
Gleb Markin <gleb.markin@banuba.com>
2021-09-15 16:23:58 +0300

    0.0.19

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+  name: "BanubaMusicEditorSDK",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "BanubaMusicEditorSDK",
+      targets: ["BanubaMusicEditorSDK"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "BanubaMusicEditorSDK",
+      path: "BanubaMusicEditorSDK.xcframework")
+  ])
